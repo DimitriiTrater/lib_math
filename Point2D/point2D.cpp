83a97cb759a6cef6294bec79d555c8277fd22318
DimitriiTrater <dima1603040@gmail.com>
2022-12-07 18:50:00 +0200

    Add point2d class

---
+++ Point2D/point2D.cpp
@@ -0,0 +1,13 @@
+#include "point2D.hpp"
+
+Point2D::Point2D()
+{
+    this->x = 0;
+    this->y = 0;
+}
+
+Point2D::Point2D(double x, double y)
+{
+    this->x = x;
+    this->y = y;
+}
---
+++ Point2D/point2D.hpp
@@ -0,0 +1,16 @@
+#ifndef POINT_2D
+#define POINT_2D
+
+class Point2D
+{
+private:
+    double x = 0;
+    double y = 0;
+
+public:
+    Point2D();
+    Point2D(double x, double y);
+};
+
+#endif
+
