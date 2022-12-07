1eb71e8745029b3f8682c8a6df55fb1a7b74ee91
DimitriiTrater <dima1603040@gmail.com>
2022-12-07 21:44:25 +0200

    Add + and - in point3d

--- Point3D/point3D.cpp
+++ Point3D/point3D.cpp
@@ -15,6 +15,22 @@ Point3D::Point3D(double x, double y, double z)
     this->z = z;
 }
 
+Point3D Point3D::operator + (const Point3D& other)
+{
+    Point3D result(0, 0, 0);
+    result.x = this->x + other.x;
+    result.y = this->y + other.y;
+    return result;
+}
+
+Point3D Point3D::operator - (const Point3D& other)
+{
+    Point3D result(0, 0, 0);
+    result.x = this->x - other.x;
+    result.y = this->y - other.y;
+    return result;
+}
+
 double Point3D::getX()
 {
     return this->x;
