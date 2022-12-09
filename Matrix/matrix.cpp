a5b13e0d686e311040b2a1b2b9da418109a9cdb9
DimitriiTrater <dima1603040@gmail.com>
2022-12-09 19:44:22 +0200

    Add in matrix type constructor and destructor

---
+++ Matrix/matrix.cpp
@@ -0,0 +1,11 @@
+#include "matrix.hpp"
+
+matrix::matrix(/* args */)
+{
+    
+}
+
+matrix::~matrix()
+{
+
+}
---
+++ Matrix/matrix.hpp
@@ -0,0 +1,13 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+class matrix
+{
+private:
+    /* data */
+public:
+    matrix(/* args */);
+    ~matrix();
+};
+
+#endif /* MATRIX_HPP */
