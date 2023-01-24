e396af34363b63df56f4e7c72860667751daee99
DimitriiTrater <dima1603040@gmail.com>
2023-01-24 22:15:02 +0200

    Static library

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
-#include "Point2D/point2D.hpp"
-#include "Vector2D/vector2D.hpp"
-#include "Vector3D/vector3D.hpp"
-#include "calc/calc.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    std::cout << ml::calc("(2+2)*2");
     return 0;
 }
